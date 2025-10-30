4cb55ffedeaf7ae7171f1549c5060cfcf93e0960
Logan Titus <Ltecho2009@gmail.com>
2025-10-29 23:31:48 -0400

    Finished the game. You can now move, and shoot, there are asteroids, asteroids kill you, and asteroids can be shot and broken up.

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 import pygame
+import sys
 
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collisions(player) == True:
+                print("Game over!")
+                sys.exit(0)
+            for shot in shots:
+                if shot.collisions(asteroid) == True:
+                    shot.kill()
+                    asteroid.split()
+        
+
         screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
